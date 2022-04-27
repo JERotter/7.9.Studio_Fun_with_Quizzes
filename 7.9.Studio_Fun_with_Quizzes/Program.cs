6cad7b3f8fafe8541f82079f8b636d854fe9333f
JERotter <49504679+JERotter@users.noreply.github.com>
2022-04-27 10:52:48 -0400

    InitCommit

---
+++ 7.9.Studio_Fun_with_Quizzes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _7._9.Studio_Fun_with_Quizzes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
